51af02ff22bb0a92313939f57fff4c8c14117279
Eirik Stenersen Sand <eiriksand@gmail.com>
2017-05-13 23:11:32 +0200

    Adding react-router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+const customHistory = createBrowserHistory();
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+);
+
+const Topic = ({match}) => (
+    <div>
+        <h3>{match.params.topicId}</h3>
+    </div>
+);
+
+const Topics = ({match}) => (
+    <div>
+        <h2>Topics</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/rendering`}>
+                    Rendering with React
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/components`}>
+                    Components
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>
+                    Props v. State
+                </Link>
+            </li>
+        </ul>
+
+        <Route path={`${match.url}/:topicId`} component={Topic}/>
+        <Route exact path={match.url} render={() => (
+            <h3>Please select a topic.</h3>
+        )}/>
+    </div>
+);
+
+const App = () => (
+    <Router history={customHistory}>
+        <div className="App">
+            <div className="AppHeader">
+                <img src={logo} className="AppLogo" alt="logo"/>
+
+                <ul className="Navigation">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/topics">Topics</Link></li>
+                </ul>
+            </div>
+
+            <div className="Content">
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/topics" component={Topics}/>
+            </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    </Router>
+);
 
-export default App;
+export default App
